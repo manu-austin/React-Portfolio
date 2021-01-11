a0288434d765fd600adf19f6fb56aab7be2c315c
Emmanuel Durand <shernua@gmail.com>
2021-01-11 14:47:20 -0600

    modified contact.js

---
+++ src/components/Pages/Contact.js
@@ -0,0 +1,40 @@
+import React from "react";
+import NavTabs from "../../components/NavTabs";
+import { Col, Row } from "react-bootstrap";
+
+
+function Contact() {
+  return (
+    // <div className="contact-container">
+
+
+    //   <div id="contact-wrapper">
+    //     <Row>
+
+    //       <Row className="contact-title jumbotron-4 justify-content-center">
+    //       <Col></Col>
+    //       <Col xs={6} className = "title-wrapper">
+    //         <Row className="justify-content-center">
+            <h1 className="display-4">Contact</h1>
+    //         </Row>
+    //       </Col>
+    //       <Col></Col>
+    //       </Row>
+
+
+    //     </Row>
+    //     <Row className="about-Nav" >
+    //       <Col></Col>
+    //       <Col xs={3} className="justify-content-center">
+    //         <NavTabs />
+    //       </Col>
+    //       <Col></Col>
+    //     </Row>
+
+    //   </div>
+    // </div >
+
+  );
+}
+
+export default Contact;
